b3228febedf112cfc50e1e69623b75ad3e872597
billybogz <billylabaguis@gmail.com>
2018-03-28 23:49:18 +0800

    assignment is finished

---
+++ webpack-babel-sample/src/app.js
@@ -0,0 +1,5 @@
+// import {kahitano, ibanaman} from "./header"
+// kahitano()
+// ibanaman()
+import checkBills() from "./check-bills"
+checkBills()
---
+++ webpack-babel-sample/src/header.js
@@ -0,0 +1,7 @@
+
+export function kahitano(){
+  console.log("I am the hghjbhjbhhb.");
+}
+export function ibanaman(){
+  console.log("Iba na to ha.");
+}
