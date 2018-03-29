b6a1b1ac03486960864de96da9c142bc2336bab3
JC <pradeljc@gmail.com>
2018-03-29 20:23:50 +0800

    new update

--- webpack-babel-sample/src/check-bills.js
+++ webpack-babel-sample/src/check-bills.js
@@ -1,11 +1,11 @@
 export function checkBills(){
   const bills = [1000, 500, 200, 50, 30]
-  let input1 = 2080
+  let input1 = 1480
   console.log("INPUT NUMBER: "+input1)
 
   bills.forEach(function(val,index){
-    const div = input1 % val
-    const count = (input1 - div) / val
+    let div = input1 % val
+    count = (input1 - div) / val
     console.log(val+": "+count)
     input1 = div
   })
